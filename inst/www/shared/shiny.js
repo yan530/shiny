58e290efab7a7845cb61a8eee077753dd660650f
Joe Cheng <joe@rstudio.com>
2017-03-12 22:53:46 -0700

    wip

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1150,18 +1150,22 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     // Message handlers =====================================================
 
     addMessageHandler('values', function (message) {
-      for (var name in this.$bindings) {
-        if (this.$bindings.hasOwnProperty(name)) this.$bindings[name].showProgress(false);
-      }
-
       for (var key in message) {
-        if (message.hasOwnProperty(key)) this.receiveOutput(key, message[key]);
+        if (message.hasOwnProperty(key)) {
+          if (this.$bindings.hasOwnProperty(key)) {
+            this.$bindings[key].showProgress(false);
+          }
+          this.receiveOutput(key, message[key]);
+        }
       }
     });
 
     addMessageHandler('errors', function (message) {
       for (var key in message) {
-        if (message.hasOwnProperty(key)) this.receiveError(key, message[key]);
+        if (message.hasOwnProperty(key)) if (this.$bindings.hasOwnProperty(key)) {
+          this.$bindings[key].showProgress(false);
+        }
+        this.receiveError(key, message[key]);
       }
     });
 
